470852b82c2eb1150a5f1c33b26b0b28d31c392d
Smit Patel <smit.patel@dal.ca>
2024-07-24 02:16:29 -0300

    update url

--- frontend/src/services/questionService.js
+++ frontend/src/services/questionService.js
@@ -1,6 +1,6 @@
 export const getQuestionBank = async () => {
     try {
-        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/questionBank`);
+        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/questionbank`);
         if(!response.ok) throw new Error('Failed to fetch question bank');
         return response.json();
     }catch (error) {
