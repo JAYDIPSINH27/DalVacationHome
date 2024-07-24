f59759a6a11479c5f1956d17ff7dca5c2dac3b74
Shubham Bhushan Pawar <sh764760@dal.ca>
2024-07-23 21:16:44 -0300

    testing 3

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useNavigate } from "react-router-dom";
 import { ReactTyped } from "react-typed";
 
+
 function LandingPage() {
 
   const navigate = useNavigate();
