88f037bcd9f00ef76329fb53b31a33f17747c2f8
Smit Patel <smit.patel@dal.ca>
2024-07-26 21:47:39 -0300

    update account id

--- infrastructure/generate_apigateway_cloudbuild.js
+++ infrastructure/generate_apigateway_cloudbuild.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const archiver = require('archiver');
 
-const AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID || "654654336786";
+const AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID || "845434235447";
 const LAMBDA_EXECUTION_ROLE =  `arn:aws:iam::${AWS_ACCOUNT_ID}:role/LabRole`;
 const LAMBDA_ZIP_DIR = '../Lambdas/output';
 const LAMBDA_CODE_BUCKET = 'csci-5410-s24-sdp-5-lambda-code';
